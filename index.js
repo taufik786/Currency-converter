b6aaaf26450570746526345788d6708df0d0a304
Taufik <taufikalam759@gmail.com>
2020-11-27 14:50:55 +0530

    Online currency converter

---
+++ index.js
@@ -0,0 +1,221 @@
+function calculate() {
+    let mainCurrency = document.getElementById('mainCurrency').value;
+    let convertCurrency = document.getElementById('convertCurrency').value;
+
+    let chooseCurrency = document.getElementById('chooseCurrency').value;
+    let convertInto = document.getElementById('convertInto').value;
+
+    if (chooseCurrency === '1') {
+        if (convertInto === '1') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1;
+        }
+        else if (convertInto === '2') {
+            document.getElementById('convertCurrency').value = mainCurrency * 62.23;
+        }
+        else if (convertInto === '3') {
+            document.getElementById('convertCurrency').value = mainCurrency * 54.27;
+        }
+        else if (convertInto === '4') {
+            document.getElementById('convertCurrency').value = mainCurrency * 3.92;
+        }
+        else if (convertInto === '5') {
+            document.getElementById('convertCurrency').value = mainCurrency * 2980.56;
+        }
+        else if (convertInto === '6') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.96;
+        }
+        else if (convertInto === '7') {
+            document.getElementById('convertCurrency').value = mainCurrency * 4.84;
+        }
+        else if (convertInto === '8') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.62;
+        }
+        else if (convertInto === '9') {
+            document.getElementById('convertCurrency').value = mainCurrency * 5.71;
+        }
+        else if (convertInto === '10') {
+            document.getElementById('convertCurrency').value = mainCurrency * 54.36;
+        }
+        else if (convertInto === '11') {
+            document.getElementById('convertCurrency').value = mainCurrency * 10372.00;
+        }
+        else if (convertInto === '12') {
+            document.getElementById('convertCurrency').value = mainCurrency * 31013.91;
+        }
+        else if (convertInto === '13') {
+            document.getElementById('convertCurrency').value = mainCurrency * 878.44;
+        }
+        else if (convertInto === '14') {
+            document.getElementById('convertCurrency').value = mainCurrency * 2.45;
+        }
+        else if (convertInto === '15') {
+            document.getElementById('convertCurrency').value = mainCurrency * 76.63;
+        }
+        else if (convertInto === '16') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.22;
+        }
+        else if (convertInto === '17') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.05;
+        }
+        else if (convertInto === '18') {
+            document.getElementById('convertCurrency').value = mainCurrency * 117.13;
+        }
+        else if (convertInto === '19') {
+            document.getElementById('convertCurrency').value = mainCurrency * 55.97;
+        }
+        else if (convertInto === '20') {
+            document.getElementById('convertCurrency').value = mainCurrency * 2.77;
+        }
+        else if (convertInto === '21') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.74;
+        }
+        
+
+    }
+    ////
+    else if(chooseCurrency === '2') {
+        if (convertInto === '1') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.016;
+        }
+        else if (convertInto === '2') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1;
+        }
+        else if (convertInto === '3') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.87;
+        }
+        else if (convertInto === '4') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.063;
+        }
+        else if (convertInto === '5') {
+            document.getElementById('convertCurrency').value = mainCurrency * 47.94;
+        }
+        else if (convertInto === '6') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.015;
+        }
+        else if (convertInto === '7') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.078;
+        }
+        else if (convertInto === '8') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.0099;
+        }
+        else if (convertInto === '9') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.092;
+        }
+        else if (convertInto === '10') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.87;
+        }
+        else if (convertInto === '11') {
+            document.getElementById('convertCurrency').value = mainCurrency * 166.86;
+        }
+        else if (convertInto === '12') {
+            document.getElementById('convertCurrency').value = mainCurrency * 497.17;
+        }
+        else if (convertInto === '13') {
+            document.getElementById('convertCurrency').value = mainCurrency * 14.08;
+        }
+        else if (convertInto === '14') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.039;
+        }
+        else if (convertInto === '15') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.23;
+        }
+        else if (convertInto === '16') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.0036;
+        }
+        else if (convertInto === '17') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.017;
+        }
+        else if (convertInto === '18') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.88;
+        }
+        else if (convertInto === '19') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.89;
+        }
+        else if (convertInto === '20') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.044;
+        }
+        else if (convertInto === '21') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.012;
+        }
+        
+    }
+    //////
+    else if(chooseCurrency === '3') {
+        if (convertInto === '1') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.018;
+        }
+        else if (convertInto === '2') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.15;
+        }
+        else if (convertInto === '3') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1;
+        }
+        else if (convertInto === '4') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.072;
+        }
+        else if (convertInto === '5') {
+            document.getElementById('convertCurrency').value = mainCurrency * 55.04;
+        }
+        else if (convertInto === '6') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.018;
+        }
+        else if (convertInto === '7') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.089;
+        }
+        else if (convertInto === '8') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.011;
+        }
+        else if (convertInto === '9') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.11;
+        }
+        else if (convertInto === '10') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.00;
+        }
+        else if (convertInto === '11') {
+            document.getElementById('convertCurrency').value = mainCurrency * 191.63;
+        }
+        else if (convertInto === '12') {
+            document.getElementById('convertCurrency').value = mainCurrency * 570.86;
+        }
+        else if (convertInto === '13') {
+            document.getElementById('convertCurrency').value = mainCurrency * 16.17;
+        }
+        else if (convertInto === '14') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.045;
+        }
+        else if (convertInto === '15') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.41;
+        }
+        else if (convertInto === '16') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.0041;
+        }
+        else if (convertInto === '17') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.019;
+        }
+        else if (convertInto === '18') {
+            document.getElementById('convertCurrency').value = mainCurrency * 2.16;
+        }
+        else if (convertInto === '19') {
+            document.getElementById('convertCurrency').value = mainCurrency * 1.03;
+        }
+        else if (convertInto === '20') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.051;
+        }
+        else if (convertInto === '21') {
+            document.getElementById('convertCurrency').value = mainCurrency * 0.014;
+        }
+        
+    }
+    /////
+
+    
+
+}
+
+// Clear button
+let inputs = document.querySelectorAll('input');
+let clearBtn = document.getElementById('clearBtn')
+clearBtn.addEventListener('click', ()=>{
+    inputs.forEach(input => input.value='');
+})
+ 
